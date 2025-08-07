580172342e468a8523283681f46cd17c7f7eb63d
sebasmc07 <73178900+sebasmc07@users.noreply.github.com>
2025-08-07 15:58:02 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://sebacabrera.github.io',
   integrations: [
     react(),
     tailwind({
